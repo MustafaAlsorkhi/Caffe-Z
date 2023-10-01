d5ac73881e0aa38eddc0a2aaaf023d2a70076b76
MustafaAlsorkhi <mustafaalsorakhi6@gmail.com>
2023-10-01 21:54:22 +0300

    popup boxes

--- app.js
+++ app.js
@@ -3,17 +3,27 @@ while (name == null || name == "") {
     name = window.prompt("Please enter your name");
   }  
   
-  var gender = window.prompt("Please enter your gender , Male or Female");
-  while (gender == null || gender == "") {
-    gender = window.prompt("Please enter your gender , Male or Female");
-  }  
-  if (gender == "Male" || gender=="male") {
-    window.alert("Mr " + name +",\nWelcome to our family");
-} else if (gender == "Female" || gender=="female") {
-    window.alert("Ms " + name + ",\nWelcome to our family");
-} else {
-    window.alert(name + " welcome to our family");
-}
+  var gender;
+
+function greetUser() {
+    
+
+    while (gender !== "male" && gender !== "female" && gender !== "Male" && gender !== "Female") {
+      gender = window.prompt("Please enter your gender, Male or Female");
+    }
+  
+    if (gender === "Male" || gender === "male") {
+      window.alert("Mr " + name + ",\nWelcome to our family");
+    } else if (gender === "Female" || gender === "female") {
+      window.alert("Ms " + name + ",\nWelcome to our family");
+    } else {
+      window.alert(name + " welcome to our family");
+    }
+
+  }
+  greetUser();
+
+  
 var drink = window.prompt(name +" do you want a hot or cold drink ?");
 
 while(drink == null || drink==""){
@@ -27,7 +37,12 @@ while (DrinkWant == null || DrinkWant == "") {
 
 window.alert("The " + DrinkWant+ " is being prepared");
 
-console.log("Your Name: "+ name + "\nYour order: " + DrinkWant);
+ const data = [name,gender,drink,DrinkWant];
+ for( i=0 ; i<data.length ; i++){
+    console.log(data[i]);
+ }
+
+//console.log("Your Name: "+ name + "\nYour order: " + DrinkWant);
 
 
 
